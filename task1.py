97e9769289bcaa66d6bd0b5559b1508c944c8003
Shaikmosin <73395830+Shaikmosin@users.noreply.github.com>
2021-11-21 17:58:51 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,42 @@
+
+
+#task 1
+#shaik Mosin
+#1
+for i in range(5,0,-1):
+    for j in range(i):
+        print(5,end=" ")
+    print()
+    
+print()
+    
+#2
+for i in range(6,1,-1):
+    for j in range(i):
+        print(j,end=" ")
+        
+    print()
+    
+print()
+#3
+k=1
+for i in range(1,6):
+    for j in range(i):
+        print(k,end=" ")
+    k+=2
+    print()
+
+print()
+
+
+#4
+for i in range(5):
+     k=i+1
+     for j in range(i+1):
+         print(k,end=" ")
+         k-=1
+     print()
+     
+    
+    
+ 
